4870bbda5d9ae46e92cbe00f59db255d0ee678f9
Shaker Alzaidy <sh.alzaidy1@gmail.com>
2021-01-05 23:49:30 -0600

    Init commit

---
+++ Detect-Pangram.py
@@ -0,0 +1,17 @@
+import string
+
+def is_pangram(s):
+    
+    allLetters = string.ascii_lowercase
+    
+    for letter in allLetters:
+        if letter not in s.lower():
+            return False
+    return True
+    
+
+
+
+pangram = "The quick, brown fox jumps over the lazy dog!"
+s = is_pangram(pangram)
+print(s)
